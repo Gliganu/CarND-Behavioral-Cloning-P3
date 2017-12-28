dcdadaedd5c2dc0cebaccba1ee882a78b9e1b0ee
Gliga Bogdan <bogdan.gliga@telenav.com>
2017-12-29 00:26:28 +0200

    add video

--- drive.py
+++ drive.py
@@ -1,80 +1,125 @@
-import argparse
+import numpy as np
+import os
+import pandas as pd
+import PIL
+import PIL.Image
+from sklearn.utils import shuffle
+from matplotlib import pyplot as plt
+
+from keras.models import Sequential, Model
+from keras.layers import Input, merge, Activation
+from keras.layers.core import Flatten, Dense, Dropout, Lambda
+from keras.layers.normalization import BatchNormalization
+from keras.optimizers import Adam
+from keras.metrics import categorical_crossentropy, categorical_accuracy
+from keras.layers.convolutional import *
+from keras.callbacks import CSVLogger, ModelCheckpoint
+
 import base64
 from datetime import datetime
 import os
 import shutil
-
 import numpy as np
 import socketio
-import eventlet
 import eventlet.wsgi
 from PIL import Image
 from flask import Flask
 from io import BytesIO
+import argparse
 
-from keras.models import load_model
-import h5py
-from keras import __version__ as keras_version
+#set min/max speed for our autonomous car
+MAX_SPEED = 25
+MIN_SPEED = 10
 
+#and a speed limit
+speed_limit = MAX_SPEED
+
+
+#initialize our server
 sio = socketio.Server()
+#our flask (web) app
 app = Flask(__name__)
-model = None
-prev_image_array = None
 
 
-class SimplePIController:
-    def __init__(self, Kp, Ki):
-        self.Kp = Kp
-        self.Ki = Ki
-        self.set_point = 0.
-        self.error = 0.
-        self.integral = 0.
+def crop(image):
+    return image[60:-25, :, :]
 
-    def set_desired(self, desired):
-        self.set_point = desired
+def get_model():
+    
+    img_size = (75, 320)
+    
+    model = Sequential()
+    model.add(Lambda(lambda x: x/127.5-1.0, input_shape=(3,) + img_size, output_shape = (3,) + img_size))
+    model.add(Convolution2D(24, 5, 5, activation='elu', subsample=(2, 2)))
+    model.add(BatchNormalization(axis=1))
+    
+    model.add(Convolution2D(36, 5, 5, activation='elu', subsample=(2, 2)))
+    model.add(BatchNormalization(axis=1))
+    
+    model.add(Convolution2D(48, 5, 5, activation='elu', subsample=(2, 2)))
+    model.add(BatchNormalization(axis=1))
+    
+    model.add(Convolution2D(64, 3, 3, activation='elu'))
+    model.add(BatchNormalization(axis=1))
+    
+    model.add(Convolution2D(64, 3, 3, activation='elu'))
+    model.add(BatchNormalization(axis=1))
+    
+    model.add(Flatten())
+    
+    model.add(Dense(100, activation='elu'))
+    model.add(Dropout(0.5))
+    
+    model.add(Dense(50, activation='elu'))
+    model.add(Dropout(0.5))
+    
+    model.add(Dense(10, activation='elu'))
+    model.add(Dropout(0.5))
+    
+    model.add(Dense(1))
 
-    def update(self, measurement):
-        # proportional error
-        self.error = self.set_point - measurement
-
-        # integral error
-        self.integral += self.error
-
-        return self.Kp * self.error + self.Ki * self.integral
+    model.compile(loss='mean_squared_error', optimizer=Adam(lr=0.0001))
+    
+    
+    return model
 
 
-controller = SimplePIController(0.1, 0.002)
-set_speed = 9
-controller.set_desired(set_speed)
-
-
+#registering event handler for the server
 @sio.on('telemetry')
 def telemetry(sid, data):
     if data:
         # The current steering angle of the car
-        steering_angle = data["steering_angle"]
-        # The current throttle of the car
-        throttle = data["throttle"]
+        steering_angle = float(data["steering_angle"])
+        # The current throttle of the car, how hard to push peddle
+        throttle = float(data["throttle"])
         # The current speed of the car
-        speed = data["speed"]
+        speed = float(data["speed"])
         # The current image from the center camera of the car
-        imgString = data["image"]
-        image = Image.open(BytesIO(base64.b64decode(imgString)))
-        image_array = np.asarray(image)
-        steering_angle = float(model.predict(image_array[None, :, :, :], batch_size=1))
+        image = Image.open(BytesIO(base64.b64decode(data["image"])))
+        try:
+            image = np.asarray(image)       # from PIL image to numpy array
+            image = crop(image) # apply the preprocessing
+            image = np.array([image])       # the model expects 4D array
+            image = np.transpose(image,(0,3,1,2))
+            
+            # predict the steering angle for the image
+            steering_angle = float(model.predict(image, batch_size=1))
+            # lower the throttle as the speed increases
+            # if the speed is above the current speed limit, we are on a downhill.
+            # make sure we slow down first and then go back to the original max speed.
+            global speed_limit
+            if speed > speed_limit:
+                speed_limit = MIN_SPEED  # slow down
+            else:
+                speed_limit = MAX_SPEED
+            throttle = 1.0 - steering_angle**2 - (speed/speed_limit)**2
 
-        throttle = controller.update(float(speed))
-
-        print(steering_angle, throttle)
-        send_control(steering_angle, throttle)
-
-        # save frame
-        if args.image_folder != '':
-            timestamp = datetime.utcnow().strftime('%Y_%m_%d_%H_%M_%S_%f')[:-3]
-            image_filename = os.path.join(args.image_folder, timestamp)
-            image.save('{}.jpg'.format(image_filename))
+            print('{} {} {}'.format(steering_angle, throttle, speed))
+            send_control(steering_angle, throttle)
+        except Exception as e:
+            print(e)
     else:
-        # NOTE: DON'T EDIT THIS.
+        
         sio.emit('manual', data={}, skip_sid=True)
 
 
@@ -92,46 +137,21 @@ def send_control(steering_angle, throttle):
             'throttle': throttle.__str__()
         },
         skip_sid=True)
-
-
+    
 if __name__ == '__main__':
+    
     parser = argparse.ArgumentParser(description='Remote Driving')
     parser.add_argument(
         'model',
         type=str,
         help='Path to model h5 file. Model should be on the same path.'
     )
-    parser.add_argument(
-        'image_folder',
-        type=str,
-        nargs='?',
-        default='',
-        help='Path to image folder. This is where the images from the run will be saved.'
-    )
+    
     args = parser.parse_args()
 
-    # check that model Keras version is same as local Keras version
-    f = h5py.File(args.model, mode='r')
-    model_version = f.attrs.get('keras_version')
-    keras_version = str(keras_version).encode('utf8')
-
-    if model_version != keras_version:
-        print('You are using Keras version ', keras_version,
-              ', but the model was built using ', model_version)
-
-    model = load_model(args.model)
-
-    if args.image_folder != '':
-        print("Creating image folder at {}".format(args.image_folder))
-        if not os.path.exists(args.image_folder):
-            os.makedirs(args.image_folder)
-        else:
-            shutil.rmtree(args.image_folder)
-            os.makedirs(args.image_folder)
-        print("RECORDING THIS RUN ...")
-    else:
-        print("NOT RECORDING THIS RUN ...")
-
+    model = get_model()
+    model.load_weights(args.model)
+    
     # wrap Flask application with engineio's middleware
     app = socketio.Middleware(sio, app)
 
